1158a9dc8f94b5ccf7496613c9522554fcba69e4
AVZ <alex.zelenskiy@yahoo.com>
2019-07-03 06:49:46 +0300

    something from git

---
+++ public/smartys/cache/547cae09e721d236220a547b1373eee3aeb68c17.first.tpl.php
@@ -0,0 +1,50 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-05-19 13:56:12
+  from 'F:\OpenServer5.3.0\OSPanel\domains\alien.loc\public\smartys\templates\first.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5ce1364c42bcf0_94428453',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '65acad60a2d8e5266762e146517e3445750f69f6' => 
+    array (
+      0 => 'F:\\OpenServer5.3.0\\OSPanel\\domains\\alien.loc\\public\\smartys\\templates\\first.tpl',
+      1 => 1558263371,
+      2 => 'file',
+    ),
+    '083d6f3274e3d403fb5b7d4d426d57dd3fbd38d0' => 
+    array (
+      0 => 'F:\\OpenServer5.3.0\\OSPanel\\domains\\alien.loc\\public\\smartys\\templates\\first_header.tpl',
+      1 => 1558252329,
+      2 => 'file',
+    ),
+    '234b028cbfd84fdc01f2342c4b697678036b8371' => 
+    array (
+      0 => 'F:\\OpenServer5.3.0\\OSPanel\\domains\\alien.loc\\public\\smartys\\templates\\first_footer.tpl',
+      1 => 1558252329,
+      2 => 'file',
+    ),
+  ),
+  'cache_lifetime' => 3600,
+),true)) {
+function content_5ce1364c42bcf0_94428453 (Smarty_Internal_Template $_smarty_tpl) {
+?><p>it's testing of include header files</p>
+<p> Test of variables insert <br> Hello. It's me - 'your header variable'  </p><h1>Some h1 header for tests only</h1>
+<h2> Hello i'm Small horse </h2>
+
+
+<p>
+    Does Small horse want to eat? - Pony says Igogogogogo </p>
+<p> Would pone eat sugar - No way.
+</p>
+<p>Would pone eat grass - Ok, i'll eat it.
+
+
+</p>
+<hr>
+<p>It's just firs footer.tpl file. I created it for testing. If you see it - IT'S ALIVE!!!</p>
+<p> Test of footer variables insert <br> Hello. It's me - 'your footer variable' </p><?php }
+}
---
+++ public/smartys/templates_c/9ee5f07a82cd335b8f49f881cf75e54b667518f4_0.file.parsing1.tpl.cache.php
@@ -0,0 +1,45 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-05-21 15:30:37
+  from 'F:\OpenServer5.3.0\OSPanel\domains\alien.loc\public\smartys\templates\parsing1.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5ce3ef6df25087_18892835',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '9ee5f07a82cd335b8f49f881cf75e54b667518f4' => 
+    array (
+      0 => 'F:\\OpenServer5.3.0\\OSPanel\\domains\\alien.loc\\public\\smartys\\templates\\parsing1.tpl',
+      1 => 1558441836,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5ce3ef6df25087_18892835 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '19333756165ce3ef6de805f7_92986323';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Parsing ONE</title>
+</head>
+<body>
+
+<pre>
+    <?php echo $_smarty_tpl->tpl_vars['matches']->value;?>
+
+</pre>
+
+
+</body>
+</html><?php }
+}
